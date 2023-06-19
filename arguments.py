44b4655293d1ba8f728b0001faf7c909736b5513
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-06-18 21:30:42 -0300

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -5,14 +5,14 @@ class Arguments:
         self._parser = argparse.ArgumentParser(description='Arguments for TreeGAN.')
 
         # Dataset argument
-        self._parser.add_argument('--dataset_path', type=str, default='C:/dataset/ShapeNet_Benchmark', help='Dataset file path.')
+        self._parser.add_argument('--dataset_path', type=str, default='/content/shapenetcore_partanno_segmentation_benchmark_v0', help='Dataset file path.')
         self._parser.add_argument('--class_choice', type=str, help='Select one class to generate. [plane, chair, ...] (default:all_class)')
-        self._parser.add_argument('--batch_size', type=int, default=10, help='Integer value for batch size.')
+        self._parser.add_argument('--batch_size', type=int, default=5, help='Integer value for batch size.')
         self._parser.add_argument('--point_num', type=int, default=2048, help='Integer value for number of points.')
 
         # Training argument
         self._parser.add_argument('--gpu', type=int, default=0, help='GPU number to use.')
-        self._parser.add_argument('--epochs', type=int, default=3000, help='Integer value for epochs.')
+        self._parser.add_argument('--epochs', type=int, default=1000, help='Integer value for epochs.')
         self._parser.add_argument('--lr', type=float, default=1e-4, help='Float value for learning rate.')
         self._parser.add_argument('--ckpt_path', type=str, default='./model/checkpoints/', help='Checkpoint path.')
         self._parser.add_argument('--ckpt_save', type=str, default='tree_ckpt_', help='Checkpoint name to save.')
