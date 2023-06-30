ffea4a43c13d1c1f5e36270dccd745a83bddae2b
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-06-30 12:02:19 -0300

    Update dataset_benchmark.py

--- data/dataset_benchmark.py
+++ data/dataset_benchmark.py
@@ -6,7 +6,7 @@ import torch
 import numpy as np
 
 class BenchmarkDataset(data.Dataset):
-    def __init__(self, root, npoints=2500, uniform=False, classification=False, class_choice=None):
+    def __init__(self, root, npoints=2500, uniform=False, classification=False, class_choice= 'plane'):
         self.npoints = npoints
         self.root = root
         self.catfile = os.path.join(self.root, 'synsetoffset2category.txt')
