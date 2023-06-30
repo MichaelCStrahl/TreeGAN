fbac48cdb01a5de7abfc384e4739a07499ee832e
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-06-30 12:29:11 -0300

    Update dataset_benchmark.py

--- data/dataset_benchmark.py
+++ data/dataset_benchmark.py
@@ -21,8 +21,10 @@ class BenchmarkDataset(data.Dataset):
                 ls = line.strip().split()
                 self.cat[ls[0]] = ls[1]
                 
-        if not class_choice is  None:
-            self.cat = {k:v for k,v in self.cat.items() if k in class_choice}
+        #if not class_choice is  None:
+           # self.cat = {k:v for k,v in self.cat.items() if k in class_choice}
+        if class_choice is not None:
+            self.cat = {k: v for k, v in self.cat.items() if v in class_choice}
 
         self.meta = {}
         for item in self.cat:
