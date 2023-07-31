8bcd36b13d4dd3401c1fc88a70d0ab591cdcb584
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-07-31 14:09:50 -0300

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -17,7 +17,7 @@ class Arguments:
         self._parser.add_argument('--lr', type=float, default=1e-4, help='Float value for learning rate.')
         self._parser.add_argument('--ckpt_path', type=str, default='/content/drive/MyDrive/ResultadosLothar/Checkpoints/', help='Checkpoint path.')
         self._parser.add_argument('--ckpt_save', type=str, default='tree_3class_ckpt_', help='Checkpoint name to save.')
-        self._parser.add_argument('--ckpt_load', type=str, default='tree_3class_ckpt_57.pt', help='Checkpoint name to load.')
+        self._parser.add_argument('--ckpt_load', type=str, default='tree_3class_ckpt_520.pt', help='Checkpoint name to load.')
         self._parser.add_argument('--visdom_port', type=int, default=8097, help='Visdom port number. (default:8097)')
         self._parser.add_argument('--visdom_color', type=int, default=4, help='Number of colors for visdom pointcloud visualization. (default:4)')
 
