d670a33ae4c0905aacedc4890a4d4a4bcb7f676c
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-08-23 18:16:09 -0300

    Update gan_network.py

--- model/gan_network.py
+++ model/gan_network.py
@@ -5,6 +5,25 @@ from layers.gcn import TreeGCN
 
 from math import ceil
 
+CATEGORIES = {
+    #'Airplane': 0,
+    #'Bag': 1,
+    'Cap': 2,
+   # 'Car': 3,
+    #'Chair': 4,
+    'Earphone': 5,
+   ## 'Guitar': 6,
+   # 'Knife': 7,
+   # 'Lamp': 8,
+   # 'Laptop': 9,
+   ## 'Motorbike': 10,
+    #'Mug': 11,
+   # 'Pistol': 12,
+   ## 'Rocket': 13,
+    #'Skateboard': 14,
+   # 'Table': 15
+    }
+
 class Discriminator(nn.Module):
     def __init__(self, batch_size, features):
         self.batch_size = batch_size
